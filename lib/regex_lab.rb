1d2433fe307e4372c85bca69758037cfb68980f5
Andrew Williams <1williamsaj@gmail.com>
2019-08-27 01:55:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,8 @@
 def starts_with_a_vowel?(word)
   if word.match(/\b[AaEeIiOoUu]/)
     return true
-  end
+  elsif word.match(/\b[^AaEeIiOoUu]/)
+
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
