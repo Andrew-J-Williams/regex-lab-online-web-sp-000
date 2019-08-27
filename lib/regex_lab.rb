223c14a30820b203aa46be70ee5793f1a0c8f0c4
Andrew Williams <1williamsaj@gmail.com>
2019-08-27 01:32:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.scan(/\W[aeiou]/)
-    return true
-  elsif word.scan(/\W[^aeiou]/)
-    return false
-  end
+  word.scan(/\W[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
