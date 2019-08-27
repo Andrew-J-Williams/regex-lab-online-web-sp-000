99dd41f43f11a416393d56cbe012a0a097c26a3d
Andrew Williams <1williamsaj@gmail.com>
2019-08-27 02:02:52 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\b+un\w+ing/)
+  text.scan(/\b+un\w+ing/) #checks the beginning of a word boundry for 'un' and then takes all 'un' words and searches for 'ing'.
 end
 
 def words_five_letters_long(text)
